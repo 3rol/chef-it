6abf69719c9e9a9d007ba5ead8b73214748fd5d3
Erol Mahmutovic <erol.mahmutovic74@gmail.com>
2023-09-08 04:31:05 +0200

    fix

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { AuthenticationService } from './../../services/authentication.service';
+import { UserService } from './../../services/user.service';
+import { UserData } from './../../models/user.model';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/models/recipe.model';
+import { RecipeService } from 'src/services/recipe.service';
+import { Comment } from 'src/models/comment.model';
+ 
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit {
+  recipeData!: Recipe;
+  comments: Comment[] = [];
+  newCommentText: string = '';
 
+  constructor(
+    private recipeService: RecipeService,
+    public authService: AuthenticationService
+  ) {}
+
+  ngOnInit(): void {
+    this.recipeService.recipeById.subscribe((result) => {
+      this.recipeData = result;
+      this.loadCommentsForRecipe();
+    });
+  }
+
+  loadCommentsForRecipe() {
+    this.recipeService.getCommentsForRecipe(this.recipeData.id).subscribe(
+      (comments) => {
+        this.comments = comments;
+      },
+      (error) => {
+        console.error('Error fetching comments:', error);
+      }
+    );
+  }
+
+  postComment(commentText: string) {
+    const recipeId = this.recipeData.id;
+    const currentUser = this.authService.currentUser || new UserData(0, '', ''); // Provide a default UserData object if currentUser is null
+    const newComment: Comment = {
+      id: 0, // You can set an initial ID here or leave it as 0
+      user: currentUser, // Use currentUser or the default UserData
+      body: commentText,
+      recipe: this.recipeData,
+    };
+  
+    this.recipeService.postComment(recipeId, newComment).subscribe((createdComment) => {
+      // After posting the comment, refresh the comments list
+      this.recipeService.getCommentsForRecipe(recipeId);
+    });
+  }
 }
