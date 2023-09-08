354f2c0b071a62acf34d697dc1ed15e136bba1e8
Erol Mahmutovic <erol.mahmutovic74@gmail.com>
2023-09-08 04:27:24 +0200

    fixes to login

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { RecipeService } from 'src/services/recipe.service';
+import { RecipeData } from 'src/models/recipe.model';
 
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
-  styleUrls: ['./add-recipe.component.css']
+  styleUrls: ['./add-recipe.component.css'],
 })
-export class AddRecipeComponent {
+export class AddRecipeComponent implements OnInit {
+  selectedRecipe: RecipeData = { name: '', description: '', recipe_image: '' };
+  selectedImage: File | null = null;
+  @ViewChild('imageInput') imageInput!: ElementRef;
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  
+  onImageSelected(event: any) {
+    this.selectedImage = event.target.files[0];
+  }
+
+  addRecipe = () => {
+    
+    if (this.selectedImage) {
+      
+      const formData = new FormData();
+      formData.append('name', this.selectedRecipe.name);
+      formData.append('description', this.selectedRecipe.description);
+      formData.append('recipe_image', this.selectedImage);
+
+      
+      this.recipeService.CreateRecipe(formData).subscribe(
+        (data: any) => {
+         
+          console.log('Recipe created:', data);
+          
+          this.selectedRecipe = { name: '', description: '', recipe_image: '' };
+         
+          this.imageInput.nativeElement.value = '';
+          this.selectedImage = null;
+        },
+        (error) => {
+          console.log('Error:', error);
+        }
+      );
+    } else {
+    
+      this.recipeService.CreateRecipe(this.selectedRecipe).subscribe(
+        (data: any) => {
+      
+          console.log('Recipe created:', data);
+          this.selectedRecipe = { name: '', description: '', recipe_image: '' };
+        },
+        (error) => {
+          console.log('Error:', error);
+        }
+      );
+    }
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/services/authentication.service';
+import { UserService } from 'src/services/user.service';
+
+interface LoginData {
+  username: string;
+  password: string;
+  
+}
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  login: LoginData = {
+    username: '',
+    password: '',
+   
+  };
+  
+  constructor(private userService: UserService, private authService: AuthenticationService){}
+
+  ngOnInit(): void {
+    this.login= {
+      username: '',
+      password: ''
+    };
+    
+  }
+  onLogin(){
+    this.userService.loginUser(this.login).subscribe(result =>{
+      this.userService.loginUser = result;
+      const userData = result;
+
+      this.authService.setUser(userData);
+
+    })
+      
+  }
+  
+  
 }
